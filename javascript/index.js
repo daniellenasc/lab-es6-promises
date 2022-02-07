38ee182e97d595c461778004a758f46fb383df12
Danielle Nascimento <danielle_nascimento_@hotmail.com>
2022-02-07 22:50:56 +0100

    add iterartion

--- javascript/index.js
+++ javascript/index.js
@@ -48,13 +48,15 @@ getInstruction('mashedPotatoes', 0, (step0) => {
                   document.querySelector("#mashedPotatoes").innerHTML += `<li>${step4}</li>`;
 
                     getInstruction('mashedPotatoes', 5, (step5) => {
-                      document.querySelector("#mashedPotatoes").innerHTML += `<li>${step5}</li>`;
+                      document.querySelector("#mashedPotatoes").innerHTML += `<li>${step5}</li>`;                      
                     });
                 });
             });
         });
     });
 });
+
+
 
 
 
@@ -114,7 +116,54 @@ obtainInstruction('steak', 0)
 
 
 // Iteration 3 using async/await
-// ...
+broccoli.push("Broccoli is ready!");
+
+async function makeBroccoli() {
+  await obtainInstruction('broccoli', 0) 
+    .then ( (step0) => {
+      document.querySelector("#broccoli").innerHTML += `<li>${step0}</li>`
+    });
+
+    await obtainInstruction('broccoli', 1) 
+    .then ( (step1) => {
+      document.querySelector("#broccoli").innerHTML += `<li>${step1}</li>`
+    });
+
+    await obtainInstruction('broccoli', 2) 
+    .then ( (step2) => {
+      document.querySelector("#broccoli").innerHTML += `<li>${step2}</li>`
+    });
+
+    await obtainInstruction('broccoli', 3) 
+    .then ( (step3) => {
+      document.querySelector("#broccoli").innerHTML += `<li>${step3}</li>`
+    });
+
+    await obtainInstruction('broccoli', 4) 
+    .then ( (step4) => {
+      document.querySelector("#broccoli").innerHTML += `<li>${step4}</li>`
+    });
+
+    await obtainInstruction('broccoli', 5) 
+    .then ( (step5) => {
+      document.querySelector("#broccoli").innerHTML += `<li>${step5}</li>`
+    });
+
+    await obtainInstruction('broccoli', 6) 
+    .then ( (step6) => {
+      document.querySelector("#broccoli").innerHTML += `<li>${step6}</li>`
+    });
+
+    await obtainInstruction('broccoli', 7) 
+    .then ( (step7) => {
+      document.querySelector("#broccoli").innerHTML += `<li>${step7}</li>`
+    });   
+}
+
+makeBroccoli()
+
+
+
 
 // Bonus 2 - Promise all
 // ...
